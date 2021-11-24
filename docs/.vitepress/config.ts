b9b7120909be6c7ef04f28bd1c22bcf022fc8f59
zjp693 <942680978@qq.com>
2021-11-24 23:14:33 +0800

    updata

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,6 @@
 export default {
   lang: 'en-US',
-  title: 'Zhang\'s blog',
+  title: 'Zhang\'s  blog',
   description: '有限的生命，只有不断的思考，才能实现无限的价值',
   // head: [ // 注入到当前页面的 HTML <head> 中的标签
   //   ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
@@ -58,8 +58,9 @@ function getGuideSidebar() {
       children: [
         { text: '1. promise 原理', link: '/documents/01_promise' },
         { text: '2. Vue基本使用', link: '/documents/02_bbb' },
+
         { text: '3. mysql80安装教程', link: '/documents/03_mysql80安装教程' },
-        // { text: '4. 指令', link: '/documents/Vue/directive' },
+        { text: '4. 指令', link: '/documents/Vue/1111' },
         // { text: '5. 计算属性', link: '/documents/Vue/computed' },
         // { text: '6. 侦听器', link: '/documents/Vue/watch' }
       ]
