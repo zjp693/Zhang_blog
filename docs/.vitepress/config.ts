51ccbc516e39274e33809f1866913d2aff1747df
zjp693 <942680978@qq.com>
2022-03-31 09:05:04 +0800

    修改自动化部署

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@ export default {
   lang: 'en-US',
   title: 'bunnyfairy',
   description: 'bunnyfairy technical documentation',
-  base: "/BunnyFairy",
+  base: "/",
   // markdown文件设置
   markdown: {
     lineNumbers: true
