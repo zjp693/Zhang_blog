e25ff6c05e4bf43a1a7b915b21229ed68942f071
zjp693 <942680978@qq.com>
2022-06-15 22:04:06 +0800

    update

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -53,6 +53,7 @@ function getGuideSidebar() {
     },
     {
       text: "前端基础",
+
       children: [
         { text: '1.HTML篇', link: '/preparation/前端基础/01.HTML篇' },
         { text: '2.CSS篇', link: '/preparation/前端基础/02.CSS篇' },
